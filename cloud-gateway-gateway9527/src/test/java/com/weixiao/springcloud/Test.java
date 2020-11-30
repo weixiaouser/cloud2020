860a314e4d382fbf121cc054ae7784050175cd41
WeiXiao <weixiao930101@163.com>
2020-11-30 21:41:09 +0800

    gateway predicates 断言

---
+++ cloud-gateway-gateway9527/src/test/java/com/weixiao/springcloud/Test.java
@@ -0,0 +1,17 @@
+package com.weixiao.springcloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author :weixiao
+ * @description :
+ * @date :2020/11/30 21:07
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+
+    }
+}
