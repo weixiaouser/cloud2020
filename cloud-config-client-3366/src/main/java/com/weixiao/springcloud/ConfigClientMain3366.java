c41c36c4598e33416cfbab7167645e9bc3fe74d9
WeiXiao <weixiao930101@163.com>
2020-12-01 17:01:00 +0800

    spring cloud config 配置中心加消息总线bus

---
+++ cloud-config-client-3366/src/main/java/com/weixiao/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.weixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author :weixiao
+ * @description :
+ * @date :2020/12/1 16:04
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/weixiao/springcloud/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package com.weixiao.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :weixiao
+ * @description :从3344这个项目获取到github上的配置信息
+ * @date :2020/12/1 14:02
+ */
+@RestController
+@RefreshScope   //刷新功能
+public class ConfigController {
+
+    @Value("${config.info}")
+    private String configInfo;
+    @Value(("${server.port}"))
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+
+        return "serverPort:"+serverPort+" configInfo:"+configInfo;
+    }
+}
