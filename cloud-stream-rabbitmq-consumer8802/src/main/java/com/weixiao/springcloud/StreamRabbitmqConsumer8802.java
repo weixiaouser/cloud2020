2408f057bc59d1dff252fda6adb153a1a9b73966
WeiXiao <weixiao930101@163.com>
2020-12-02 16:49:11 +0800

    Spring cloud stream 消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/weixiao/springcloud/StreamRabbitmqConsumer8802.java
@@ -0,0 +1,17 @@
+package com.weixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :weixiao
+ * @description :
+ * @date :2020/12/2 16:33
+ */
+@SpringBootApplication
+public class StreamRabbitmqConsumer8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitmqConsumer8802.class,args);
+    }
+}
