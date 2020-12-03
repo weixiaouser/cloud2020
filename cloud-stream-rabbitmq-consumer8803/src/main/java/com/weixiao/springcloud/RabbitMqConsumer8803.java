4c966929e7f4b7a878d2e68b64c5d4d1a5dfde18
WeiXiao <weixiao930101@163.com>
2020-12-03 10:52:54 +0800

    Spring Cloud Stream

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/weixiao/springcloud/RabbitMqConsumer8803.java
@@ -0,0 +1,17 @@
+package com.weixiao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :weixiao
+ * @description :
+ * @date :2020/12/3 9:58
+ */
+@SpringBootApplication
+public class RabbitMqConsumer8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqConsumer8803.class,args);
+    }
+}
